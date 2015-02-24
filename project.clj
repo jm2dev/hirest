722c538fe276c1e6a13c93f4a4644abb1b804b72
jm2dev <jomarcar@gmail.com>
2015-02-24 20:00:40 +0000

    Adding liberator dependency.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.3.1"]]
+                 [ring-server "0.3.1"]
+                 [liberator "0.12.2"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler hirest.handler/app
          :init hirest.handler/init
